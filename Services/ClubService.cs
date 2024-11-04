684a41f0c390ef76308a02667beb2c380f0a80d1
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-04 12:08:17 +0100

    ClubService implemented

--- Services/ClubService.cs
+++ Services/ClubService.cs
@@ -1,48 +1,73 @@
 using AutoMapper;
+using CloudinaryDotNet.Actions;
 using RunningGroupAPI.DTOs.Club;
 using RunningGroupAPI.Interfaces.Repositories;
 using RunningGroupAPI.Interfaces.Services;
+using RunningGroupAPI.Models;
 
 namespace RunningGroupAPI.Services;
 
 public class ClubService : IClubService
 {
-    private readonly IMapper _mapper;
-    private readonly IClubRepository _clubRepository;
+	private readonly IMapper _mapper;
+	private readonly IClubRepository _clubRepository;
+	private readonly IPhotoService _photoService;
 
-	public ClubService(IMapper mapper, IClubRepository clubRepository)
+	public ClubService(IMapper mapper, IClubRepository clubRepository, IPhotoService photoService)
 	{
 		_mapper = mapper;
 		_clubRepository = clubRepository;
+		_photoService = photoService;
 	}
 
-	public Task<IEnumerable<ClubDTO>> GetAllClubsAsync()
+	public async Task<IEnumerable<ClubDTO>> GetAllClubsAsync()
 	{
-		throw new NotImplementedException();
+		var clubs = await _clubRepository.GetAllClubsAsync();
+		return _mapper.Map<IEnumerable<ClubDTO>>(clubs);
 	}
 
-	public Task<ClubDTO> GetClubByIdAsync(int id)
+	public async Task<ClubDTO> GetClubByIdAsync(int id)
 	{
-		throw new NotImplementedException();
+		var club = await _clubRepository.GetClubByIdAsync(id);
+		return club != null ? _mapper.Map<ClubDTO>(club) : null;
 	}
 
-	public Task<IEnumerable<ClubDTO>> GetClubsByCityAsync(string city)
+	public async Task<IEnumerable<ClubDTO>> GetClubsByCityAsync(string city)
 	{
-		throw new NotImplementedException();
+		var clubs = await _clubRepository.GetClubsByCityAsync(city);
+		return _mapper.Map<IEnumerable<ClubDTO>>(clubs);
 	}
 
-	public bool AddClub(CreateClubDTO ceateClubDto)
+	public bool AddClub(CreateClubDTO createClubDto)
 	{
-		throw new NotImplementedException();
+		ImageUploadResult imageUpload = _photoService.AddPhotoAsync(createClubDto.Image).Result; 
+		if(imageUpload == null || imageUpload.Error != null) return false;
+		
+		var club = _mapper.Map<Club>(createClubDto);
+		club.Image = imageUpload.Url.ToString();
+		
+		return _clubRepository.AddClub(club);
 	}
 	public bool UpdateClub(int id, UpdateClubDTO updateClubDto)
 	{
-		throw new NotImplementedException();
+		var ogClub = _clubRepository.GetClubByIdAsync(id).Result;
+		if(ogClub == null) return false;
+		
+		var updatedClub = _mapper.Map(updateClubDto, ogClub);
+		if(updateClubDto.Image != null)
+		{
+			var result = _photoService.AddPhotoAsync(updateClubDto.Image).Result;
+			if(result == null || result.Error != null) return false;
+			
+			updatedClub.Image = result.Url.ToString();
+		}
+		
+		return _clubRepository.UpdateClub(updatedClub);
 	}
 	
 	public bool RemoveClub(int id)
 	{
-		throw new NotImplementedException();
+		return _clubRepository.RemoveClub(id);
 	}
 
 }
