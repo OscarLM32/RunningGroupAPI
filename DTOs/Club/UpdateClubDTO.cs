dc10a2c7198ca0391e27d43626a7418d756a2939
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-04 11:37:44 +0100

    Pass IFormFile instead of image url

--- DTOs/Club/UpdateClubDTO.cs
+++ DTOs/Club/UpdateClubDTO.cs
@@ -6,7 +6,7 @@ public class UpdateClubDTO
 {
 	public string? Title { get; set; }
 	public string? Description { get; set; }
-	public string? Image { get; set; }
+	public IFormFile? Image { get; set; }
 
 	public string? Country { get; set; }
 	public string? City { get; set; }
