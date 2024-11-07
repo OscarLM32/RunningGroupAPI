3e56a5d124045471afc1dd31d7f53e95c823297a
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-08 00:34:52 +0100

    AppUser model update

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -7,14 +7,12 @@ public class AppUser : IdentityUser
 {
 	public int? Pace { get; set; }
 	public int? Mileage { get; set; } 
-	public string? ProfileImageUrl {get; set;}
+	public string ProfileImageUrl {get; set;}
 
-	[ForeignKey("Address")]
-	public int? AddressId { get; set; }
-	public Address? Address { get; set; }
+	public string? Country { get; set; }
 	public string? City { get; set; }
-	public string? State {get; set; }
 
-	public ICollection<Club> Clubs { get; set; }
-	public ICollection<Race> Races { get; set; }
+	public ICollection<ClubMembership> ClubMemberships { get; set; } = new List<ClubMembership>();
+	public ICollection<RaceParticipant> RaceParticipants { get; set; } = new List<RaceParticipant>();
+
 }
